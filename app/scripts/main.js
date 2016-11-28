999b026895b69bbad2e901684a65add7d636a7b0
zchan0 <zchan0@outlook.com>
2016-11-28 15:10:30 -0500

    Sad commit

---
+++ app/scripts/main.js
@@ -0,0 +1,39 @@
+// login & signup
+
+$('#signupBtn').click(function () {
+	 let data   = $('form').serialize();
+	 let signup = $.ajax({
+	 	type: 'POST',
+	 	data: data,
+	 	dataType: 'JSON',
+	 	url: 'user/register.json',
+	 	success: function(resultsData, status) {
+	 		let results = JSON.parse(resultsData);
+	 		if (results.success === 'true') {
+	 			console.log('signup success');
+	 			window.location.href = 'timeline.html';
+	 		} else if (results.success === 'false') {
+	 			console.log('signup failure');
+	 		}
+	 	}
+	 }); 
+});
+
+$('#loginBtn').click(function () {
+	 let data  = $('form').serialize();
+	 let login = $.ajax({
+	 	type: 'POST',
+	 	data: data,
+	 	dataType: 'JSON',
+	 	url: 'user/login.json',
+	 	success: function(resultsData, status) {
+	 		let results = JSON.parse(resultsData);
+	 		if (results.success === 'true') {
+	 			console.log('login success');
+	 			window.location.href = 'timeline.html';
+	 		} else if (results.success === 'false') {
+	 			console.log('login failure');
+	 		}
+	 	}
+	 });
+});
