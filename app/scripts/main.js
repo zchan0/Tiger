c3c627a5aaab09db9d293c7656c998be09b55426
zchan0 <zchan0@outlook.com>
2016-11-28 19:47:55 -0500

    Make it more beautiful

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,13 +38,30 @@ $('#loginBtn').click(function () {
 	 });
 });
 
-// init Masonry
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
-});
-// layout Isotope after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry();
-});  
+(function( $ ) {
+
+	var $container = $('.masonry-container');
+	$container.imagesLoaded( function () {
+		$container.masonry({
+			columnWidth: '.item',
+			itemSelector: '.item'
+		});
+	});
+	
+	//Reinitialize masonry inside each panel after the relative tab link is clicked - 
+	$('a[data-toggle=tab]').each(function () {
+		var $this = $(this);
+
+		$this.on('shown.bs.tab', function () {
+		
+			$container.imagesLoaded( function () {
+				$container.masonry({
+					columnWidth: '.item',
+					itemSelector: '.item'
+				});
+			});
+
+		}); //end shown
+	});  //end each
+	
+})(jQuery);
