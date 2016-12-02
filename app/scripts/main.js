855ff3476f182e70cc3710100b2189534b2c792b
sqct2008 <sqct2008@gmail.com>
2016-12-02 18:44:04 -0500

    almost useable

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -61,11 +61,7 @@ $('#logoutBtn').click(function () {
 
 //get init data function
 function getAllContent(){
-    let data = {
-        start:"0",
-        pagesize:"10"
-    };
-    data = JSON.stringify(data);
+    let data = 'start=0&pagesize=10';
     console.log('data:',data);
 
     $.ajax({
@@ -74,74 +70,110 @@ function getAllContent(){
         dataType: 'JSON',
         url: 'content/batchquery.json',
         success: function (resultsData, status){
+            console.log('jsonstring',resultsData);
+
 	 		let results = JSON.parse(resultsData);
-	 		if (results.success === 'true') {
+            console.log('success',results.success);
+            console.log('myContents',results.myContents);
+            console.log('results',results);
+
+            //container
+            let $container = $('#gridContainer');
+            $container.masonry({
+                ifFitWidth: true,
+                itemSelector: '.col-md-4 col-sm-6 item'
+            });
+
+            if (results.success === 'true') {
                 console.log('batchquery success');
+
                 let myContents = results.myContents;
-                let document = document.getElementById('container');
                 console.log('mycontents',myContents);
-                for(i=0;i<contents.length;i++){
+
+                for(i=0;i<myContents.length;i++){
                     //element i
                     let contents = myContents[i];
+                    console.log('contents',contents);
 
+                    for(j=0;j<contents.contents.length;j++){
+
+                        //element of share i
+                        let item = document.createElement('div');
+                        item.setAttribute('class','col-md-4 col-sm-6 item');
+
+                        let thumbnail = document.createElement('div');
+                        thumbnail.setAttribute('class','thumbnail');
+
+                        let caption = document.createElement('div')
+                        caption.setAttribute('class','caption');
+
+                        let h3 = document.createElement('h3');
+                        let p1 = document.createElement('p');
+                        let p2 = document.createElement('p');
+
+                        let a1 = document.createElement('a');
+                        a1.setAttribute('href','#');
+                        a1.setAttribute('class','btn btn-default');
+                        a1.setAttribute('role','button');
+                        a1.setAttribute('id','selectBtn');
+                        a1.innerHTML = 'select';
+                        p2.appendChild(a1);
+
+                        let a2 = document.createElement('a');
+                        a2.setAttribute('href','#');
+                        a2.setAttribute('class','btn btn-danger');
+                        a2.setAttribute('role','button');
+                        a2.setAttribute('id','deleteBtn');
+                        a2.innerHTML = 'select';
+                        a2.innerHTML = 'button';
+                        p2.appendChild(a2);
+                        //end of basic set of DOM!
+
+                        //start to input contents to DOM!
+                        p1.innerHTML = contents.contents[j].text;
+                        h3.innerHTML = 'Description';
+
+                        caption.appendChild(h3);
+                        caption.appendChild(p1);
+                        caption.appendChild(p2);
+
+                        //if has picture, add img element!
+                        if(contents.contents[j].hasOwnProperty('picName')){
+                            console.log('have picture');
+                            let image = document.createElement('img');
+                            image.setAttribute('alt','');
+                            let src = '/yolk/pic/download.json?username='+contents.sharedByUsername+'&fileName='+contents.contents[j].picName;
+                            image.setAttribute('src',src);
+
+                            thumbnail.appendChild(image);
+                        }
+                        else{
+                            console.log("don't have image");
+                        }
+                        //end of adding img
+
+                        thumbnail.appendChild(caption);
+                        item.appendChild(thumbnail);
+                        item.setAttribute('id',contents.id);
+                        //end of input contents to DOM!
+
+                        //use masonry to add new item
+                        $container.masonry().append(item).masonry('appended',item);
+                    }
                     //basic set of DOM!
-                    let item = document.createElement('div');
-                    item.setAttribute('class','col-md-4 col-sm-6 item');
 
-                    let thumbnail = document.createElement('div');
-                    thumbnail.setAttribute('class','thumbnail');
-
-                    let image = document.createElement('img');
-                    image.setAttribute('alt','');
-
-                    let caption = document.createElement('div')
-                    caption.setAttribute('class','caption');
-
-                    let h3 = document.createElement('h3');
-                    let p1 = document.createElement('p');
-                    let p2 = document.createElement('p');
-
-                    let a1 = document.createElement('a');
-                    a1.setAttribute('href','#');
-                    a1.setAttribute('class','btn btn-default');
-                    a1.setAttribute('role','button');
-                    a1.setAttribute('id','selectBtn');
-                    a1.innerHTML = 'select';
-                    p2.appendChild(a);
-
-                    let a2 = document.createElement('a');
-                    a1.setAttribute('href','#');
-                    a1.setAttribute('class','btn btn-alert');
-                    a1.setAttribute('role','button');
-                    a1.setAttribute('id','deleteBtn');
-                    a1.innerHTML = 'select';
-                    a2.innerHTML = 'button';
-                    p2.appendChild(a);
-                    //end of basic set of DOM!
-
-                    //start to input contents to DOM!
-                    p1.innerHTML = contents.contents[0].text;
-                    h3.innerHTML = 'Label';
-
-                    caption.appendChild(h3);
-                    caption.appendChild(p1);
-                    caption.appendChild(p2);
-
-                    image.setAttribute('src',contents.contents[0].picName);
-
-                    thumbnail.appendChild(image);
-                    thumbnail.appendChild(caption);
-
-                    item.appendChild(thumbnail);
-                    item.setAttribute('id',contents.id);
-                    //end of input contents to DOM!
-
-                    document.getElementById('gridContainer').appendChild(item);
+                    //add a divider of each share. not working???
+                    let ul = document.createElement('ul');
+                    ul.setAttribute('class','nav nav-list');
+                    let divider = document.createElement('li');
+                    divider.setAttribute('class','divider');
+                    ul.appendChild(divider);
+                    $container.masonry().append(ul).masonry('appended',ul);
                 }
-	 		} else if (results.success === 'false') {
+            } else if (results.success === 'false') {
                 console.log('batchquery failure');
                 alert('cannot get contents!');
-	 		}
+            }
         }
     });
 }
