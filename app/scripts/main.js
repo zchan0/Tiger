0a17e1ab8c192e8e28298b41d8d2a06c9b0e6ef5
zchan0 <zchan0@outlook.com>
2016-12-01 20:37:24 -0500

    Fix compile error

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -92,57 +92,57 @@ function getAllContent(){
 
                     //basic set of DOM!
                     let item = document.createElement('div');
-                    item.setAttribute("class","col-md-4 col-sm-6 item");
+                    item.setAttribute('class','col-md-4 col-sm-6 item');
 
                     let thumbnail = document.createElement('div');
-                    thumbnail.setAttribute("class","thumbnail");
+                    thumbnail.setAttribute('class','thumbnail');
 
                     let image = document.createElement('img');
-                    image.setAttribute("alt","");
+                    image.setAttribute('alt','');
 
                     let caption = document.createElement('div')
-                    caption.setAttribute("class","caption");
+                    caption.setAttribute('class','caption');
 
                     let h3 = document.createElement('h3');
                     let p1 = document.createElement('p');
                     let p2 = document.createElement('p');
 
                     let a1 = document.createElement('a');
-                    a1.setAttribute("href","#");
-                    a1.setAttribute("class","btn btn-default");
-                    a1.setAttribute("role","button");
-                    a1.setAttribute("id","selectBtn");
-                    a1.innerHTML = "select";
+                    a1.setAttribute('href','#');
+                    a1.setAttribute('class','btn btn-default');
+                    a1.setAttribute('role','button');
+                    a1.setAttribute('id','selectBtn');
+                    a1.innerHTML = 'select';
                     p2.appendChild(a);
 
                     let a2 = document.createElement('a');
-                    a1.setAttribute("href","#");
-                    a1.setAttribute("class","btn btn-alert");
-                    a1.setAttribute("role","button");
-                    a1.setAttribute("id","deleteBtn");
-                    a1.innerHTML = "select";
-                    a2.innerHTML = "button";
+                    a1.setAttribute('href','#');
+                    a1.setAttribute('class','btn btn-alert');
+                    a1.setAttribute('role','button');
+                    a1.setAttribute('id','deleteBtn');
+                    a1.innerHTML = 'select';
+                    a2.innerHTML = 'button';
                     p2.appendChild(a);
                     //end of basic set of DOM!
 
                     //start to input contents to DOM!
                     p1.innerHTML = contents.contents[0].text;
-                    h3.innerHTML = "Label";
+                    h3.innerHTML = 'Label';
 
                     caption.appendChild(h3);
                     caption.appendChild(p1);
                     caption.appendChild(p2);
 
-                    image.setAttribute("src",contents.contents[0].picName);
+                    image.setAttribute('src',contents.contents[0].picName);
 
                     thumbnail.appendChild(image);
                     thumbnail.appendChild(caption);
 
                     item.appendChild(thumbnail);
-                    item.setAttribute("id",contents.id);
+                    item.setAttribute('id',contents.id);
                     //end of input contents to DOM!
 
-                    document.getElementById("gridContainer").appendChild(item);
+                    document.getElementById('gridContainer').appendChild(item);
                 }
 	 		} else if (results.success === 'false') {
                 console.log('batchquery failure');
@@ -183,7 +183,7 @@ $('#uploadBtn').click(function () {
 });
 
 //show modal uploading function
-$("#loading")
+$('#loading')
     .ajaxStart(function () {
         $(this).show();
     })//when uploading images, show the icon
@@ -199,7 +199,6 @@ $('#alertDiv').click(function () {
 });
 
 (function( $ ) {
-
 	var $container = $('.masonry-container');
 	$container.imagesLoaded().progress( function () {
 		$container.masonry({
@@ -207,24 +206,4 @@ $('#alertDiv').click(function () {
 			itemSelector: '.item'
 		});
 	});
-
-	//Reinitialize masonry inside each panel after the relative tab link is clicked -
-	$('a[data-toggle=tab]').each(function () {
-		var $this = $(this);
-
-		$this.on('shown.bs.tab', function () {
-
-			$container.imagesLoaded( function () {
-		
-			$container.imagesLoaded().progress( function () {
-				$container.masonry({
-					columnWidth: '.item',
-					itemSelector: '.item'
-				});
-			});
-
-		}); //end shown
-	});  //end each
-
-})(jQuery);
-
+}) (jQuery);
